7a5d1c387903bc9e116444a7bde47cd1bbfaa930
radu andrada <radu andrada>
2017-08-17 23:01:57 +0300

    tentativa de active link

---
+++ internship-web/src/static/js/navbar.js
@@ -0,0 +1,15 @@
+function changeClass(){
+	console.log($("a[href*='" + location.pathname + "']")[0].pathname);
+	var path = $("a[href*='" + location.pathname + "']")[0].pathname;
+	var liNodes = $('#nav li');
+
+	var len = liNodes.length;
+	console.log(liNodes);
+	console.log(len);
+	for (var i = 0; i < len; i++) {
+		if (liNodes[i].id === path){
+			console.log(liNodes[i]);
+			liNodes[i].className += " active-page";
+		}
+	}
+}
